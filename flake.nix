a2d54684872765c708e64735b88ba7101cc4ab40
Thive-N <35178093+Thive-N@users.noreply.github.com>
2025-06-25 01:07:38 +0100

    Removing laptop declaration

--- flake.nix
+++ flake.nix
@@ -20,23 +20,6 @@
 
     in {
       nixosConfigurations = {
-        desktop = lib.nixosSystem {
-          inherit system;
-          modules = [ 
-            ./hosts/desktop/configuration.nix
-
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.thivejan = ./home/desktop/home.nix;
-            }
-
-            hyprland.nixosModules.default {
-              programs.hyprland.enable = true;
-            }
-          ];
-        };
-
         laptop = lib.nixosSystem {
           inherit system;
           modules = [
