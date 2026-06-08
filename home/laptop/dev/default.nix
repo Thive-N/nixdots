43dbe9e829df6625370b07210fce2df40c198e89
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-06-09 00:18:47 +0100

    resolving neovim warning

--- home/laptop/dev/default.nix
+++ home/laptop/dev/default.nix
@@ -14,6 +14,7 @@
   programs.neovim.viAlias = true;
   programs.neovim.vimAlias = true;
   programs.neovim.withPython3 = true;
+  programs.neovim.withRuby = true;
 
   programs.vscode = {
     enable = true;
